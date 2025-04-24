b012ab799d1e401d08aab641df552e521af71b6f
Rahul <rahulkrishnappa94@gmail.com>
2025-04-24 12:49:28 +0530

    added morgan for logging

--- controllers/register.js
+++ controllers/register.js
@@ -1,13 +1,14 @@
 const handleRegister = (req, res, db, bcrypt) => {
-  const { email, name, password } = req.body;
+  const { email, name, password } = req.body
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('incorrect form submission')
   }
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
+  const hash = bcrypt.hashSync(password)
+  db.transaction(trx => {
+    trx
+      .insert({
         hash: hash,
-        email: email
+        email: email,
       })
       .into('login')
       .returning('email')
@@ -21,20 +22,17 @@ const handleRegister = (req, res, db, bcrypt) => {
             // loginEmail[0].email --> this now returns the email
             email: loginEmail[0].email,
             name: name,
-            joined: new Date()
+            joined: new Date(),
           })
           .then(user => {
-            res.json(user[0]);
+            res.json(user[0])
           })
       })
       .then(trx.commit)
       .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
+  }).catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
-  handleRegister: handleRegister
-};
-
-
+  handleRegister: handleRegister,
+}
